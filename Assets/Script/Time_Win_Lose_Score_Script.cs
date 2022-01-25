49a08a606f3e343a7b04a94f384f04effd24ee81
KanouIzumi <72852474+KanouIzumi@users.noreply.github.com>
2022-01-25 17:44:42 +0800

    Added Score and Timer Text
    The score text can work but I am still working on the timer and it's text

---
+++ Assets/Script/Time_Win_Lose_Score_Script.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Time_Win_Lose_Score_Script : MonoBehaviour
+{
+    private AudioSource m_AudioSource;
+    [SerializeField] private AudioClip m_GetCoin;           // the sound played when character touches the coin.
+
+    //This is for the score 
+    public Text scoreText;
+    private GameObject[] coins;
+    private int score;
+
+    //this is for the time
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_AudioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        WinCondition();
+    }
+
+    private void PlayGetCoinsound()
+    {
+        m_AudioSource.clip = m_GetCoin;
+        m_AudioSource.Play();
+    }
+
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        if (collision.gameObject.tag == "Coin")
+        {
+            score +=10;
+            scoreText.text = "Score: " + score;
+            PlayGetCoinsound();
+            Destroy(collision.gameObject);
+        }
+
+        if (collision.gameObject.tag == "Water")
+        {
+            print("You touch Water");
+            SceneManager.LoadScene("GameLoseScene");
+        }
+
+    }
+
+    //How to win the game
+    private void WinCondition()
+    {
+        coins = GameObject.FindGameObjectsWithTag("Coin");
+
+        if (coins.Length <= 0)
+        {
+            SceneManager.LoadScene("GameWinScene");
+        }
+    }
+}
